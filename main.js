cd4ffad43040bac0eac209a0cd92654302947280
Mohamed Abdeldayem <98813428+abdeldayem8@users.noreply.github.com>
2024-12-09 00:42:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector("nav ul");
+  
+    menuToggle.addEventListener("click", () => {
+      menu.classList.toggle("show");
+  
+      menuToggle.classList.toggle("active");
+    });
+  });
